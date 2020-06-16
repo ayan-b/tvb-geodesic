f011684f7ec1c830eab8b631de1c184623df23c6
Ayan <ayanbanerjee7777@gmail.com>
2020-06-16 19:22:46 +0530

    Use ctypes

--- gdist.py
+++ gdist.py
@@ -1,4 +1,4 @@
-from ctypes import POINTER, CDLL, c_int, c_uint, c_double
+from ctypes import POINTER, addressof, CDLL, c_int, c_uint, c_double
 import glob
 
 import numpy as np
@@ -8,22 +8,32 @@ libfile = glob.glob('build/*/gdist*.so')[0]
 
 lib = CDLL(libfile)
 
+lib.compute_gdist.argtypes = [
+    c_uint,
+    c_uint,
+    np.ctypeslib.ndpointer(dtype=np.float64),
+    np.ctypeslib.ndpointer(dtype=np.int32),
+    c_uint,
+    c_uint,
+    np.ctypeslib.ndpointer(dtype=np.int32),
+    np.ctypeslib.ndpointer(dtype=np.int32),
+    c_double,
+]
+
+lib.compute_gdist.restype = POINTER(c_double)
+
+lib.local_gdist_matrix.argtypes = [
+    c_uint,
+    c_uint,
+    np.ctypeslib.ndpointer(dtype=np.float64),
+    np.ctypeslib.ndpointer(dtype=np.int32),
+    POINTER(c_uint),
+    c_double,
+]
+lib.local_gdist_matrix.restype = POINTER(c_double)
+
 
 class Gdist(object):
-    def __init__(self):
-        lib.compute_gdist.argtypes = [
-            c_uint,
-            c_uint,
-            np.ctypeslib.ndpointer(dtype=np.float64),
-            np.ctypeslib.ndpointer(dtype=np.int32),
-            c_uint,
-            c_uint,
-            np.ctypeslib.ndpointer(dtype=np.int32),
-            np.ctypeslib.ndpointer(dtype=np.int32),
-            c_double,
-        ]
-        lib.compute_gdist.restype = POINTER(c_double)
-
     def compute_gdist(
         self,
         number_of_vertices,
@@ -47,6 +57,27 @@ class Gdist(object):
             target_indices_array,
             distance_limit
         )
+
+    def local_gdist_matrix(
+        self,
+        number_of_vertices,
+        number_of_triangles,
+        vertices,
+        triangles,
+        max_distance,
+    ):
+        sparse_matrix_size = 0
+        data_size = lib.local_gdist_matrix(
+            number_of_vertices,
+            number_of_triangles,
+            vertices,
+            triangles,
+            addressof(c_int(sparse_matrix_size)),
+            max_distance
+        )
+        
+        data = np.fromiter(data, dtype=np.float64, count=sparse_matrix_size)
+        return data
 
 
 def compute_gdist(
@@ -74,3 +105,29 @@ def compute_gdist(
         distance_limit=max_distance
     )
     return np.fromiter(distance, dtype=np.float64, count=target_indices.size)
+
+
+def local_gdist_matrix(
+    vertices,
+    triangles,
+    max_distance = 10**100
+):
+    vertices = vertices.ravel()
+    triangles = triangles.ravel()
+    rows = POINTER(c_uint)()
+    columns = POINTER(c_uint)()
+    data = POINTER(c_double)()
+
+    g = Gdist()
+    data = g.local_gdist_matrix(
+        vertices.size,
+        triangles.size,
+        vertices,
+        triangles,
+        max_distance
+    )
+    print(data)
+
+    # return scipy.sparse.csc_matrix(
+    #     (data, (rows, columns)), shape=(vertices.size, vertices.size)
+    # )
--- gdist_c_api.cpp
+++ gdist_c_api.cpp
@@ -38,12 +38,63 @@ double* compute_gdist_impl(
 
     algorithm.propagate(all_sources, distance_limit, &stop_points);
 
-    // double distance[target_indices.size()];
-    double *distance = (double *)malloc(sizeof(double) * target_indices.size());
+    double *distance = new double[target_indices.size()];
     for (int i = 0; i < (int)stop_points.size(); ++i) {
         algorithm.best_source(stop_points[i], distance[i]);
     }
     return distance;
+}
+
+double* local_gdist_matrix_impl(
+    unsigned number_of_vertices,
+    unsigned number_of_triangles,
+    double *vertices,
+    unsigned *triangles,
+    unsigned *sparse_matrix_size,
+    double max_distance
+) {
+    std::vector<double> points (vertices, vertices + number_of_vertices);
+    std::vector<unsigned> faces (triangles, triangles + number_of_triangles);
+    
+    geodesic::Mesh mesh;
+    mesh.initialize_mesh_data(points, faces); // create internal mesh data structure including edges
+
+    geodesic::GeodesicAlgorithmExact algorithm(&mesh); // create exact algorithm for the mesh
+
+    std::vector<unsigned> rows_vector, columns_vector;
+    std::vector <double> data_vector;
+
+    double distance = 0;
+
+    std::vector<geodesic::SurfacePoint> targets(number_of_vertices), source(1);
+
+    for (int i = 0; i < number_of_vertices; ++i) {
+        targets[i] = geodesic::SurfacePoint(&mesh.vertices()[i]);
+    }
+
+    for (int i = 0; i < number_of_vertices; ++i) {
+        source[0] = geodesic::SurfacePoint(&mesh.vertices()[i]);
+        algorithm.propagate(source, max_distance);
+        for (int j = 0; j < number_of_vertices; ++j) {
+            algorithm.best_source(targets[j], distance);
+
+            if (distance != geodesic::GEODESIC_INF and distance != 0) {
+                rows_vector.push_back(i);
+                columns_vector.push_back(j);
+                data_vector.push_back(distance);
+            }
+        }
+    }
+
+    double *data = new double[data_vector.size() * 3];
+
+    *sparse_matrix_size = data_vector.size();
+
+    std::copy(rows_vector.begin(), rows_vector.end(), data);
+    std::copy(columns_vector.begin(), columns_vector.end(), data + data_vector.size());
+    std::copy(data_vector.begin(), data_vector.end(), data + 2 * data_vector.size());
+
+    return data;
 }
 
 
@@ -69,6 +120,24 @@ extern "C" {
             source_indices_array,
             target_indices_array,
             distance_limit
+        );
+    }
+
+    double* local_gdist_matrix(
+        unsigned number_of_vertices,
+        unsigned number_of_triangles,
+        double *vertices,
+        unsigned *triangles,
+        unsigned *sparse_matrix_size,
+        double max_distance
+    ) {
+        return local_gdist_matrix_impl(
+            number_of_vertices,
+            number_of_triangles,
+            vertices,
+            triangles,
+            sparse_matrix_size,
+            max_distance
         );
     }
 };
